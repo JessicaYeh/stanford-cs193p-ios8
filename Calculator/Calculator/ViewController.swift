15aad45fb27a7fc4fcf210e061291cd51055d3e7
Jessica Yeh <jyeh@smu.edu>
2015-02-19 16:34:35 -0600

    Lecture 1: Logistics, iOS 8 Overview

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jessica Yeh on 2/19/15.
+//  Copyright (c) 2015 Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+}
+
