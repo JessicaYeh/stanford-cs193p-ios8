30ecc5207453839c97e83c9f55ea83c177f91898
Jessica Yeh <jyeh@smu.edu>
2015-02-22 13:54:56 -0600

    Lecture 5: Objective-C Compatibility, Property List, Views

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -56,7 +56,6 @@ class ViewController: UIViewController {
         }
         set {
             display.text = "\(newValue)"
-            //userIsInTheMiddleOfTypingANumber = false
         }
     }
     
---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Jessica Yeh on 2/22/15.
+//  Copyright (c) 2015 Yeh. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
