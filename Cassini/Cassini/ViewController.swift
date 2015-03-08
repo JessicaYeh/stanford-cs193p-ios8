fa24a1508e515cc3ed90b6d9293d74eff4ff0c0d
Jessica Yeh <jyeh@smu.edu>
2015-03-07 20:05:30 -0600

    Lecture 9: Scroll View and Multithreading

---
+++ Cassini/Cassini/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Jessica Yeh on 2/28/15.
+//  Copyright (c) 2015 Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController {
+            if let identifier = segue.identifier {
+                switch identifier {
+                case "Earth":
+                    ivc.imageURL = DemoURL.NASA.Earth
+                    ivc.title = "Earth"
+                case "Saturn":
+                    ivc.imageURL = DemoURL.NASA.Saturn
+                    ivc.title = "Saturn"
+                case "Cassini":
+                    ivc.imageURL = DemoURL.NASA.Cassini
+                    ivc.title = "Cassini"
+                default: break
+                }
+            }
+        }
+    }
+
+}
+
